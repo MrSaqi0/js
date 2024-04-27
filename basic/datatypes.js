05e87e48ad8e2fa7002e9293c02faa67c99bbd12
mr saqi <168265705+MrSaqi0@users.noreply.github.com>
2024-04-27 10:55:04 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,12 @@
+/*
+number = 12345
+string = ""
+boolean = true/false
+null = standalone value
+undefind =
+bigint
+symbol = unique
+*/
+console.log(typeof "18");
+console.log(typeof 18);
+console.log(typeof null);
