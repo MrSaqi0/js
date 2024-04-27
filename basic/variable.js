bd99e682e97326597e1de752bfa16f66c971545c
mr saqi <168265705+MrSaqi0@users.noreply.github.com>
2024-04-27 10:55:29 +0000

    variable

---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "mrsaqi009@gmail.com"
+var accountCity ="gujranwale"
+accountPassword = " 1234"
+let accountState  ;
+
+// accountId= 122113
+
+accountEmail = "vsbhdjk@gmail"
+accountCity = "lahore"
+accountPassword = "22222"
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
