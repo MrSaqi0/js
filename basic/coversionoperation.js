770d9b6ceff21ffa168b0e5241220eb1832b0858
mr saqi <168265705+MrSaqi0@users.noreply.github.com>
2024-04-27 11:18:02 +0000

    converonoperation

---
+++ basic/coversionoperation.js
@@ -0,0 +1,28 @@
+let score = "33abc" 
+console.log(typeof score);
+
+let valueNumber = Number(score)
+
+console.log(typeof valueNumber);
+console.log(valueNumber);
+// 33 = 33
+//33abs = NaN
+// true =1
+// false =0
+
+
+
+let isLoggedIn = 1
+let booleanIsLoddedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoddedIn);
+
+//1 = true 
+//0 = false
+//""= false
+//mrsaqi = true
+
+let sumNumber = 123
+
+let stringNumber = String(sumNumber)
+
+console.log(typeof stringNumber);
