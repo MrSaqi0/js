bbdc8a152e72235020aacf2ab3b4b41465b80f47
mr saqi <168265705+MrSaqi0@users.noreply.github.com>
2024-04-29 11:27:19 +0000

    string

---
+++ basic/01-string.js
@@ -0,0 +1,6 @@
+let name = "Mr--saqi"
+let accountEmail = "saqibbashir@gmail.come"
+
+console.log(`Hello My name is ${name.toUpperCase()} and my email is ${accountEmail}`)
+console.log(name .length);
+console.log(name.charAt(4));
