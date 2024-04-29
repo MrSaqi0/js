5a4b202ab8b3a45e2234b414cfffb603f423dbed
mr saqi <168265705+MrSaqi0@users.noreply.github.com>
2024-04-29 11:27:39 +0000

    date

---
+++ basic/03-date.js
@@ -0,0 +1,6 @@
+let myDate = new Date(2000 , 4 ,28) 
+
+console.log(myDate.toDateString());
+console.log(myDate.getDate());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
