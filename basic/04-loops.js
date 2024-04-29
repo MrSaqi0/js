3480416225ce9d9a4f286cf54a371344f6ff280b
mr saqi <168265705+MrSaqi0@users.noreply.github.com>
2024-04-29 13:47:45 +0000

    loops

---
+++ basic/04-loops.js
@@ -0,0 +1,23 @@
+// for loop
+
+for (let i = 8; i <= 8; i++) {
+    for (let o = 1; o <= 10; o++)  {
+        
+        console.log(i + '*' + o + '=' + i*o);
+        
+   }
+    
+}
+
+// while loop
+
+while (condition) {
+    
+}
+
+
+//do while loop
+
+do {
+    
+} while (condition);
